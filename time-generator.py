bb4d3f91f7171be324bdbd5f996b9f0013656f3c
butterfly <38087594+ithabibi@users.noreply.github.com>
2024-07-11 22:45:44 +0330

    developing

--- time-generator.py
+++ time-generator.py
@@ -4,7 +4,7 @@ from dateutil.relativedelta import relativedelta
 
 start_date = datetime.date(2020, 1, 1)
 end_date = datetime.date(2022, 1, 4)
-delta = datetime.timedelta(days=1)
+#delta = datetime.timedelta(days=1)
 
 while start_date <= end_date:
     print(start_date)
