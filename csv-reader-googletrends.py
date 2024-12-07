e680b15173dcef33524947c99d088c3ade85e70f
butterfly <38087594+ithabibi@users.noreply.github.com>
2024-12-07 15:01:48 +0330

    comment

--- csv-reader-googletrends.py
+++ csv-reader-googletrends.py
@@ -30,8 +30,9 @@ for filename in os.listdir(directory_path):
                 # تبدیل رشته تاریخ به شیء تاریخ با فرمت مشخص
                 date_obj = datetime.strptime(date_string, '%d/%m/%y')
                 # تبدیل شیء تاریخ به رشته با فرمت yyyy-mm-dd
-                Scrape_date = date_obj.strftime('%Y-%m-%d')
-        print("Scrape date:"+Scrape_date)
+                scrape_date = date_obj.strftime('%Y-%m-%d')
+
+        print("Scrape date:"+scrape_date)
         #################################################################
         results = []  # لیست برای ذخیره نتایج
         # متغیر برای پیگیری اینکه در بخش TOP یا RISING هستیم
@@ -42,6 +43,7 @@ for filename in os.listdir(directory_path):
             if line == "":
                 continue
 
+             # شناسایی بخش‌های TOP و RISING
             if line == "TOP":
                 current_section = "TOP"
                 continue
@@ -49,13 +51,14 @@ for filename in os.listdir(directory_path):
                 current_section = "RISING"
                 continue
 
+            # ذخیره نتایج بر اساس بخش فعلی
             if current_section == "TOP":
-                results.append(f"{line},{Scrape_date}, TOP")
+                results.append(f"{line},{scrape_date}, TOP")
             elif current_section == "RISING":
-                results.append(f"{line},{Scrape_date}, RISING")
+                results.append(f"{line},{scrape_date}, RISING")
 
         # نوشتن نتایج در یک فایل جدید یا چاپ آنها
-        output_filename = Scrape_date+'.csv'
+        output_filename = scrape_date+'.csv'
         with open(output_filename, 'w', encoding='utf-8') as output_file:
             for result in results:
                 output_file.write(result + '\n')
